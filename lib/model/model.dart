3872a8e5b7dc8377ff6b50ef5e9d35041977d303
adinanhamza <adinanck4557@gmail.com>
2025-04-10 17:31:43 +0530

    model added

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,3 +1,32 @@
 class StudentModel{
-  
+
+  String? id;
+  String name;
+  String age;
+  String address;
+  String image;
+
+  StudentModel({required this.address,required this.age, this.id,required this.image,required this.name});
+
+  factory StudentModel.fromDataBase(Map<String,dynamic>json, String id){
+return StudentModel(
+  id: id,
+  name: json['name'] ?? '',
+  age: json['age'].toString() ,
+  address: json['address'] ?? '',
+  image: json['image'] ?? '',
+
+
+);
+  }
+
+Map<String,dynamic>toDataBase(){
+return {
+'id' : id,
+'name' : name,
+'age' : age,
+'address' : address,
+'image' : image,
+};
+}
 }
